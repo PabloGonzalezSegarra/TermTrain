ea0f4446fcb425bcea544090ce075b147487a91d
Pablo <pablogscraft@gmail.com>
2025-05-02 12:57:13 +0200

    Added math.h

---
+++ src/core/math.h
@@ -0,0 +1,11 @@
+#ifndef TERM_TRAIN_MATH_INCLUDE__
+#define TERM_TRAIN_MATH_INCLUDE__
+
+#include <stdint.h>
+
+typedef struct {
+    uint32_t x;
+    uint32_t y;
+} Vect2;
+
+#endif // TERM_TRAIN_MATH_INCLUDE__
